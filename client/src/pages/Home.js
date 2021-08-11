b194558843aa8112d9e60909d4574dea33ccece7
heganjr <jacobhegan@hotmail.com>
2021-08-12 02:25:27 +0800

    added html pages

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import { useQuery } from '@apollo/client';
 
-import ThoughtList from '../components/ThoughtList';
-import ThoughtForm from '../components/ThoughtForm';
-
 import { QUERY_THOUGHTS } from '../utils/queries';
 
 const Home = () => {
@@ -17,16 +14,12 @@ const Home = () => {
           className="col-12 col-md-10 mb-3 p-3"
           style={{ border: '1px dotted #1a1a1a' }}
         >
-          <ThoughtForm />
         </div>
         <div className="col-12 col-md-8 mb-3">
           {loading ? (
             <div>Loading...</div>
           ) : (
-            <ThoughtList
-              thoughts={thoughts}
-              title="Some Feed for Thought(s)..."
-            />
+            <h1>WELCOME</h1>
           )}
         </div>
       </div>
