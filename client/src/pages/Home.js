8a9c30f23ca3745bebcc44c37a6ccae25d647555
heganjr <jacobhegan@hotmail.com>
2021-08-14 21:49:14 +0800

    improved login and signup functionality

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,26 +1,15 @@
-import React from 'react';
-import { useQuery } from '@apollo/client';
+import React from "react";
+import { useQuery } from "@apollo/client";
 
-import { QUERY_THOUGHTS } from '../utils/queries';
 
 const Home = () => {
-  const { loading, data } = useQuery(QUERY_THOUGHTS);
-  const thoughts = data?.thoughts || [];
-
   return (
     <main>
       <div className="flex-row justify-center">
-        <div
-          className="col-12 col-md-10 mb-3 p-3"
-          
-        >
-        </div>
+        <div className="col-12 col-md-10 mb-3 p-3"></div>
         <div className="col-12 col-md-8 mb-3">
-          {loading ? (
-            <div>Loading...</div>
-          ) : (
-            <h1>WELCOME</h1>
-          )}
+          {/* {loading ? <div>Loading...</div> : <h1>WELCOME</h1>} */}
+          <h1>Welcome!</h1>
         </div>
       </div>
     </main>
