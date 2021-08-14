98b0b54c94e7fe60c1f23fb9b1f776181389ddd1
heganjr <jacobhegan@hotmail.com>
2021-08-15 04:42:57 +0800

    modified home.js removed ternary operator

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -8,8 +8,7 @@ const Home = () => {
       <div className="flex-row justify-center">
         <div className="col-12 col-md-10 mb-3 p-3"></div>
         <div className="col-12 col-md-8 mb-3">
-          {/* {loading ? <div>Loading...</div> : <h1>WELCOME</h1>} */}
-          <h1>Welcome!</h1>
+          <h1>Welcome! </h1>
         </div>
       </div>
     </main>
