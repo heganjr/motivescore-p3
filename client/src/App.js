6dead72eb10052782caf45f2c0c1fd0b5709cac3
heganjr <jacobhegan@hotmail.com>
2021-08-16 01:20:36 +0800

    heroku amendment

--- client/src/App.js
+++ client/src/App.js
@@ -25,8 +25,8 @@ const authLink = setContext((_, { headers }) => {
   // get the authentication token from local storage if it exists
   const token = localStorage.getItem("id_token");
   // return the headers to the context so httpLink can read them
-  return {
   console.log('arghhhhhh', headers)
+  return {
 
     headers: {
       ...headers,
