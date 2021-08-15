6257b3816ffc8c1a91170056a5e68bd2de3d92de
heganjr <jacobhegan@hotmail.com>
2021-08-16 02:09:54 +0800

    testing heroku deployment issue removed httpLink and authlink in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,9 +3,9 @@ import {
   ApolloClient,
   InMemoryCache,
   ApolloProvider,
-  createHttpLink,
+  // createHttpLink,
 } from "@apollo/client";
-import { setContext } from "@apollo/client/link/context";
+// import { setContext } from "@apollo/client/link/context";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import Signup from "./pages/Signup";
@@ -15,30 +15,32 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
 
-// Construct our main GraphQL API endpoint
-const httpLink = createHttpLink({
-  uri: "/graphql",
-});
+// // Construct our main GraphQL API endpoint
+// const httpLink = createHttpLink({
+//   uri: "/graphql",
+// });
 
-// Construct request middleware that will attach the JWT token to every request as an `authorization` header
-const authLink = setContext((_, { headers }) => {
-  // get the authentication token from local storage if it exists
-  const token = localStorage.getItem("id_token");
-  // return the headers to the context so httpLink can read them
-  console.log('arghhhhhh', headers)
-  return {
+// // Construct request middleware that will attach the JWT token to every request as an `authorization` header
+// const authLink = setContext((_, { headers }) => {
+//   // get the authentication token from local storage if it exists
+//   const token = localStorage.getItem("id_token");
+//   // return the headers to the context so httpLink can read them
+//   console.log('arghhhhhh', headers)
+//   return {
 
-    headers: {
-      ...headers,
-      authorization: token ? `Bearer ${token}` : "",
-    },
-  };
-});
+//     headers: {
+//       ...headers,
+//       authorization: token ? `Bearer ${token}` : "",
+//     },
+//   };
+// });
 
 const client = new ApolloClient({
   // Set up our client to execute the `authLink` middleware prior to making the request to our GraphQL API
-  link: authLink.concat(httpLink),
+  // link: authLink.concat(httpLink),
+  uri: '/graphql',
   cache: new InMemoryCache(),
+  
 });
 
 function App() {
